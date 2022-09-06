ed0fba42bc93af5c0e4cdd5a77f3d93714dedb2c
MichaelMar7 <michaelmar719@gmail.com>
2022-09-06 17:05:59 +1200

    basic implementation of repository pattern

--- music/adapters/memory_repository.py
+++ music/adapters/memory_repository.py
@@ -6,6 +6,7 @@ from music.domainmodel.user import User
 from music.domainmodel.review import Review
 from music.domainmodel.playlist import PlayList
 from music.adapters.repository import AbstractRepository, RepositoryException
+from music.adapters.csvdatareader import TrackCSVReader
 
 class MemoryRepository(AbstractRepository):
     def __init__(self):
@@ -13,6 +14,52 @@ class MemoryRepository(AbstractRepository):
         self.__tracks_index = dict()
         self.__users = list()
         self.__reviews = list()
-    
+
+    def add_track(self, track: Track):
+        self.__tracks.append(track)
+        self.__tracks_index[track.track_id] = track
+
     def add_user(self, user: User):
         self.__users.append(user)
+
+    def add_review(self, review: Review):
+        # super().add_review(review)
+        self.__reviews.append(review)
+    
+    def get_user(self, user_name) -> User:
+        return next((user for user in self.__users if user.user_name == user_name), None)
+    
+    def get_track(self, id: int) -> Track:
+        track = None
+        try:
+            track = self.__tracks_index[id]
+        except KeyError:
+            pass
+        return track
+    
+    def get_number_of_tracks(self):
+        return len(self.__tracks)
+    
+    # B requirements search by methods
+    def get_tracks_by_artists(self, target_artist: Artist):
+        pass
+
+    def get_tracks_by_album(self, target_album: Album):
+        pass
+
+    def get_tracks_by_genre(self, target_genre: Genre):
+        pass
+
+    def get_first_track(self):
+        pass
+
+    def get_last_track(self):
+        pass
+
+    def get_previous_track(self, track: Track):
+        pass
+
+    def get_next_track(self, track: Track):
+        pass
+    
+
--- music/adapters/repository.py
+++ music/adapters/repository.py
@@ -1,4 +1,11 @@
 import abc
+from music.domainmodel.artist import Artist
+from music.domainmodel.album import Album
+from music.domainmodel.track import Track
+from music.domainmodel.genre import Genre
+from music.domainmodel.user import User
+from music.domainmodel.review import Review
+from music.domainmodel.playlist import PlayList
 
 repo_instance = None
 
@@ -8,4 +15,57 @@ class RepositoryException(Exception):
         pass
 
 class AbstractRepository(abc.ABC):
-    pass
+
+    @abc.abstractmethod
+    def add_track(self, track: Track):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def add_user(self, user: User):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def add_review(self, review: Review):
+        # Do extra stuff
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def get_user(self, user_name) -> User:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def get_track(self, id: int) -> Track:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def get_number_of_tracks(self):
+        raise NotImplementedError
+    
+    # B requirements search by methods
+    @abc.abstractmethod
+    def get_first_track(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_last_track(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_tracks_by_artists(self, target_artist: Artist):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_tracks_by_album(self, target_album: Album):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_tracks_by_genre(self, target_genre: Genre):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_previous_track(self, track: Track):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_next_track(self, track: Track):
+        raise NotImplementedError
