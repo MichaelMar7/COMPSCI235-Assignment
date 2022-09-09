52da8b09e38b98625fbf47f3724c0b12cde6c93f
MichaelMar7 <michaelmar719@gmail.com>
2022-09-09 12:48:59 +1200

    now getting random track from repo

--- music/__init__.py
+++ music/__init__.py
@@ -3,6 +3,8 @@
 from pathlib import Path
 from flask import Flask, render_template
 
+import random
+
 import music.adapters.repository as repo
 from music.adapters.memory_repository import MemoryRepository, populate
 
@@ -56,7 +58,28 @@ def create_app(test_config=None):
     def home():
         some_track = create_some_track()
         # Use Jinja to customize a predefined html page rendering the layout for showing a single track.
-        return render_template('simple_track.html', track=some_track)
+        return render_template('simple_track.html', track=get_random_track())
+        #return render_template('simple_track.html', track=create_some_track())
+        #return render_template('simple_track.html', track=get_random_track(), first_track = get_first_track(), track_list = get_100_tracks(0, 100), track_count = get_track_count())
     """"""
 
     return app
+
+# blueprint methods testing
+def get_first_track():
+    for track in get_100_tracks(100, 1000):
+        if track is not None:
+            return track
+
+def get_100_tracks(min, max):
+    return repo.repo_instance.get_tracks_by_id([x for x in range(min, max)])
+
+def get_track_count():
+    return repo.repo_instance.get_number_of_tracks()
+
+def get_random_track():
+    while True:
+        index = random.randint(0, 10000)
+        for track in get_100_tracks(index, index+1):
+            if track is not None:
+                return track
