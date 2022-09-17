feec794cfff84c9e6b3b1060e52744625657db5e
MichaelMar7 <michaelmar719@gmail.com>
2022-09-17 21:18:42 +1200

    now can browse through tracks

--- music/blueprints/browse/browse.py
+++ music/blueprints/browse/browse.py
@@ -34,28 +34,71 @@ def browse_tracks_by_id():
 def browse_tracks():
     target_title = request.args.get("track_title")
     target_id = request.args.get("track_id")
+
     first_track = services.get_first_track(repo.repo_instance)
     last_track = services.get_last_track(repo.repo_instance)
     if first_track is None:
-        target_title = first_track["title"]
+        target_title = first_track.track_title
+        target_id = first_track.track_id
+    
     track = first_track
+    if target_id is not None and services.get_track_by_id(target_id, repo) is not None:
+        print("a")
+        track = services.get_track_by_id(target_id, repo)
+        print(track)
     if target_title is not None and services.get_track_by_title(target_title, repo) is not None:
+        print("b")
         track = services.get_track_by_title(target_title, repo)
-    #if services.get_track(target_id, repo) is not None and target_id is not None:
-    #    track = services.get_track(target_id, repo)
+        print(track)
+    print(track)
+    
+    first_track_url = None #url_for('browse_bp.browse_tracks', track_title=first_track.title)
+    last_track_url = None #url_for('browse_bp.browse_tracks', track_title=last_track.title)
+    previous_track_url = None
+    next_track_url = None
 
-    # sidebar random album
-    random_album = utilities.get_random_album()
-    random_album_tracks = repo.repo_instance.get_tracks_by_album(random_album.title)
+    if repo.repo_instance.get_number_of_tracks() > 0:
+        previous_track = services.get_previous_track(track, repo)
+        next_track = services.get_next_track(track, repo)
+        if previous_track is not None:
+            previous_track_url = url_for('browse_bp.browse_tracks', track_title=previous_track.title)
+            first_track_url = url_for('browse_bp.browse_tracks', track_title=first_track.title)
+        if next_track is not None:
+            next_track_url = url_for('browse_bp.browse_tracks', track_title=next_track.title)
+            last_track_url = url_for('browse_bp.browse_tracks', track_title=last_track.title)
+        
+        """Testing"""
+        print(first_track)
+        print(last_track)
+        print(previous_track)
+        print(next_track)
 
-    return render_template(
-        "browse/tracks.html",
-        title="Tracks",
-        random_track=utilities.get_random_track(), 
-        random_album=random_album, 
-        random_album_tracks=random_album_tracks,
-        track_by_title_demo=track
-    )
+        print(first_track_url)
+        print(last_track_url)
+        print(previous_track_url)
+        print(next_track_url)
+        """"""
+
+        # sidebar random album
+        random_album = utilities.get_random_album()
+        random_album_tracks = repo.repo_instance.get_tracks_by_album(random_album.title)
+
+        return render_template(
+            "browse/tracks.html",
+            title="Tracks",
+            random_track=utilities.get_random_track(), 
+            random_album=random_album, 
+            random_album_tracks=random_album_tracks,
+            track=track,
+            first_track=first_track,
+            last_track=last_track,
+            first_track_url=first_track_url,
+            last_track_url=last_track_url,
+            previous_track_url=previous_track_url,
+            next_track_url=next_track_url
+
+        )
+    return redirect(url_for('home_bp.home'))
 
 @browse_blueprint.route('/browse_albums', methods=['GET'])
 def browse_albums():
